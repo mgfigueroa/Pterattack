0c81b9829e2e4b005c98b70e57cf11c7715d5d0f
mgfigueroa <mgfigueroa@ucdavis.edu>
2015-12-06 01:49:32 -0800

    Projectile pickups/upgrades

---
+++ Pterattack/Pterattack/pFireball.swift
@@ -0,0 +1,9 @@
+//
+//  pFireBall.swift
+//  Pterattack
+//
+//  Created by Michael Figueroa on 12/5/15.
+//
+//
+
+import Foundation
---
+++ Pterattack/Pterattack/pLaser.swift
@@ -0,0 +1,9 @@
+//
+//  pLaser.swift
+//  Pterattack
+//
+//  Created by Michael Figueroa on 12/5/15.
+//
+//
+
+import Foundation
